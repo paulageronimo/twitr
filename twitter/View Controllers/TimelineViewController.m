5563773e29933bbc36be5e60e3d79c5016dc1298
Paula Letícia Gerónimo <paulagero182@gmail.com>
2021-06-28 17:26:40 -0400

    user can sign in through OAuth login flow, and log out.
    also fixed the API keys. I guess it was a good way to mislead randos wanting to use my code meanwhile I was creating the gitignore

--- twitter/View Controllers/TimelineViewController.m
+++ twitter/View Controllers/TimelineViewController.m
@@ -8,6 +8,8 @@
 
 #import "TimelineViewController.h"
 #import "APIManager.h"
+#import "AppDelegate.h"
+#import "LoginViewController.h"
 
 @interface TimelineViewController ()
 
@@ -30,6 +32,17 @@
             NSLog(@"😫😫😫 Error getting home timeline: %@", error.localizedDescription);
         }
     }];
+}
+
+- (void)loggedOut {
+    // TimelineViewController.m
+    AppDelegate *appDelegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
+
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    LoginViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
+    appDelegate.window.rootViewController = loginViewController;
+    [[APIManager shared] logout];
+    
 }
 
 - (void)didReceiveMemoryWarning {
